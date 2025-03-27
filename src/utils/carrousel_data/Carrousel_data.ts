d78e3708a974ea41759c04177520c7cbc532e833
Muhammed21 <mcavus048@gmail.com>
2025-03-27 15:15:55 +0100

    :sparkles: Publication nouvelle version du site

--- src/utils/carrousel_data/Carrousel_data.ts
+++ src/utils/carrousel_data/Carrousel_data.ts
@@ -7,11 +7,11 @@ export const Carrousel_Data = [
   {
     title: "Image 2",
     description: "Description Image 2",
-    img: "/img/png/img2.png",
+    img: "/img/png/horror-img.png",
   },
   {
     title: "Image 3",
     description: "Description Image 3",
-    img: "/img/png/img3.png",
+    img: "/img/png/horror-img-2.png",
   },
 ];
